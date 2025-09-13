021f5b813717851753a6d1f2fb26dd8588d401c4
thirdlf03 <thirdlf03@gmail.com>
2025-09-13 21:57:25 +0900

    game: make canvas full-screen and remove rounded/shadow to avoid clipping

--- src/app/game/_components/GameCanvas.tsx
+++ src/app/game/_components/GameCanvas.tsx
@@ -474,12 +474,12 @@ export default function GameCanvas() {
   };
 
   return (
-    <div className="relative">
+    <div className="relative h-[100svh] w-[100vw] overflow-hidden">
       <canvas
         ref={canvasRef}
         width={CANVAS_W}
         height={CANVAS_H}
-        className="mx-auto block rounded bg-black shadow-lg"
+        className="block h-full w-full touch-none bg-black select-none"
       />
       {overlay()}
       <p className="mt-3 text-sm text-zinc-300">
